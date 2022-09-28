b064ae50addfaa215bf0750d4329c3387b8f7862
raymondaffedzie@protonmail.com <raymondaffedzie@protonmail.com>
2022-09-28 17:39:45 +0000

    first commit

---
+++ School Management System/SchoolManagementSystem/SchoolManagementSystem/Admin/AddClass.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class AddClass : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+            }
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("SELECT Row_NUMBER() OVER(ORDER BY (SELECT 1)) AS [Sr.No], ClassId, ClassName FROM Class"); 
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = fn.Fetch("SELECT * FROM Class WHERE ClassName = '" + txtClass.Text.Trim() + "'");
+                if(dt.Rows.Count == 0)
+                {
+                    string query = "INSERT INTO Class VALUES('" + txtClass.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Insert Successfully!";
+                    lblMsg.CssClass = "alert alert-success";
+                    txtClass.Text = string.Empty;
+                    GetClass();
+                }
+                else
+                {
+                    lblMsg.Text = "Class Already Exist";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+    }
+}
